01a2d3eb41217d6a6b07399e4b894e968f954b29
kaushik <kaushikcg15@gmail.com>
2025-09-06 11:31:22 +0530

    remove preperation folder 2025-2026

--- Preperation 2025-2026/removeWhiteSpace.java
+++
@@ -1,7 +0,0 @@
-class removeWhiteSpace{
-	public static void main(String[] args){
-	String str = "Remove white spaces"; 
-	String newstr = str.replaceAll("\\s+","");
-	System.out.println("New string without white space :" + newstr);
-	}
-}
